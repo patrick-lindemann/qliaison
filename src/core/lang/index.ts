5dfb453ff7f40ee8f8b3ceb16245bbe4a1f80304
Patrick Lindemann <patrick.lindemann.99@gmail.com>
2023-04-20 14:15:49 +0200

    Added new exports to lang/index.ts

--- src/core/lang/index.ts
+++ src/core/lang/index.ts
@@ -1,3 +1,5 @@
+export { Function } from './functions';
 export { Keyword, keywords } from './keywords';
-export { Operator, operators } from './operations';
-export { Type, Value, types } from './values';
+export { And, Comparison, Condition, Equals, GreaterThan, GreaterThanEquals, In, IsEmpty, IsNotEmpty, IsNotNull, IsNull, LessThan, LessThanEquals, Like, Not, NotEquals, NotIn, Operator, Or, operators } from './operations';
+export { Array, Boolean, Null, Number, String, Type, Value, types } from './values';
+export { Variable } from './variables';
