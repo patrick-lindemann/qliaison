88d0c2a04ad685193be8744855d0256815c02a21
Patrick Lindemann <patrick.lindemann.99@gmail.com>
2023-05-04 12:13:57 +0200

    Restructured code

--- src/base/ast/index.ts
+++ src/base/ast/index.ts
@@ -1,7 +1,47 @@
 export { Array } from './array';
 export { Function } from './function';
 export { AstNode } from './node';
-export { BinaryOperation, Operation, UnaryOperation } from './operation';
+export {
+  And,
+  BinaryOperation,
+  Comparable,
+  Comparator,
+  Comparison,
+  Condition,
+  Equals,
+  GreaterThan,
+  GreaterThanEquals,
+  In,
+  Inable,
+  Is,
+  IsNot,
+  Isable,
+  LessThan,
+  LessThanEquals,
+  Like,
+  Likeable,
+  LogicOperator,
+  Not,
+  NotEquals,
+  NotIn,
+  Operation,
+  Operator,
+  Or,
+  Orderable,
+  UnaryOperation,
+  comparators,
+  logicOperators
+} from './operation';
 export { Root } from './root';
-export { Value } from './value';
+export {
+  BooleanValue,
+  DateValue,
+  EmptyValue,
+  NullValue,
+  NumberValue,
+  StringValue,
+  Type,
+  Value,
+  types
+} from './value';
 export { Variable } from './variable';
--- src/base/ast/operation.ts
+++ src/base/ast/operation.ts
@@ -1,10 +1,70 @@
 import { Visitor } from 'base/visitor';
+import { Array } from './array';
 import { AstNode } from './node';
+import {
+  BooleanValue,
+  DateValue,
+  EmptyValue,
+  NullValue,
+  NumberValue,
+  StringValue,
+  Value
+} from './value';
+import { Variable } from './variable';
+
+/* Types */
+
+export type Operator = LogicOperator | Comparator;
+export type LogicOperator = (typeof logicOperators)[number];
+export type Comparator = (typeof comparators)[number];
+
+export type Condition = Not | And | Or;
+export type Comparison =
+  | Equals
+  | NotEquals
+  | LessThan
+  | LessThanEquals
+  | GreaterThan
+  | GreaterThanEquals
+  | Like
+  | Is
+  | IsNot
+  | In
+  | NotIn;
+
+export type Comparable =
+  | NullValue
+  | BooleanValue
+  | NumberValue
+  | StringValue
+  | DateValue;
+export type Orderable = NumberValue | DateValue;
+export type Likeable = StringValue;
+export type Isable = NullValue | EmptyValue;
+export type Inable = Array<Value>;
+
+/* Constants */
+
+export const logicOperators = ['not', 'and', 'or'] as const;
+
+export const comparators = [
+  'eq',
+  'neq',
+  'lt',
+  'lte',
+  'gt',
+  'gte',
+  'like',
+  'is',
+  'is_not',
+  'in',
+  'not_in'
+] as const;
 
 /* Abstract Classes */
 
 export abstract class Operation extends AstNode {
-  constructor(public operator: string) {
+  constructor(public operator: Operator) {
     super();
     this.operator = operator;
   }
@@ -13,7 +73,7 @@ export abstract class Operation extends AstNode {
 /* Classes */
 
 export class UnaryOperation extends Operation {
-  constructor(public operator: string, public right: AstNode) {
+  constructor(public operator: Operator, public right: AstNode) {
     super(operator);
     this.right = right;
   }
@@ -24,7 +84,7 @@ export class UnaryOperation extends Operation {
 
 export class BinaryOperation extends Operation {
   constructor(
-    public operator: string,
+    public operator: Operator,
     public left: AstNode,
     public right: AstNode
   ) {
@@ -34,5 +94,99 @@ export class BinaryOperation extends Operation {
   }
   accept<T>(visitor: Visitor<T>): unknown {
     return visitor.visitBinaryOperation(this);
+  }
+}
+
+/* Logical Operations */
+
+export class Not extends UnaryOperation {
+  constructor(public value: Condition | Comparison) {
+    super('not', value);
+  }
+}
+
+export class And extends BinaryOperation {
+  constructor(
+    public left: Condition | Comparison,
+    public right: Condition | Comparison
+  ) {
+    super('and', left, right);
+  }
+}
+
+export class Or extends BinaryOperation {
+  constructor(
+    public left: Condition | Comparison,
+    public right: Condition | Comparison
+  ) {
+    super('or', left, right);
+  }
+}
+
+/* Comparisons */
+
+export class Equals extends BinaryOperation {
+  constructor(public left: Variable, public right: Comparable) {
+    super('eq', left, right);
+  }
+}
+
+export class NotEquals extends BinaryOperation {
+  constructor(public left: Variable, public right: Comparable) {
+    super('neq', left, right);
+  }
+}
+
+export class LessThan extends BinaryOperation {
+  constructor(public left: Variable, public right: Orderable) {
+    super('lt', left, right);
+  }
+}
+
+export class LessThanEquals extends BinaryOperation {
+  constructor(public left: Variable, public right: Orderable) {
+    super('lte', left, right);
+  }
+}
+
+export class GreaterThan extends BinaryOperation {
+  constructor(public left: Variable, public right: Orderable) {
+    super('gt', left, right);
+  }
+}
+
+export class GreaterThanEquals extends BinaryOperation {
+  constructor(public left: Variable, public right: Orderable) {
+    super('gte', left, right);
+  }
+}
+
+export class Like extends BinaryOperation {
+  constructor(public left: Variable, public right: Likeable) {
+    super('like', left, right);
+  }
+}
+
+export class Is extends BinaryOperation {
+  constructor(public left: Variable, public right: Isable) {
+    super('is', left, right);
+  }
+}
+
+export class IsNot extends BinaryOperation {
+  constructor(public left: Variable, public right: Isable) {
+    super('is_not', left, right);
+  }
+}
+
+export class In extends BinaryOperation {
+  constructor(public left: Variable, public right: Inable) {
+    super('in', left, right);
+  }
+}
+
+export class NotIn extends BinaryOperation {
+  constructor(public left: Variable, public right: Inable) {
+    super('not_in', left, right);
   }
 }
