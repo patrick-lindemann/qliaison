d0d63f3ec8ac268a5d9696c27adbc5e7f81ad612
Patrick Lindemann <patrick.lindemann.99@gmail.com>
2023-06-16 01:52:43 +0200

    Moved jest.config.js into packages

--- packages/ag-grid/jest.config.js
+++ packages/ag-grid/jest.config.js
@@ -1,6 +1,5 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
-  moduleDirectories: ['node_modules', 'src']
+  testEnvironment: 'node'
 };
