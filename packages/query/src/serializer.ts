78fd6c10d664353b6a2d761457dc8cb7cd67dd0f
Patrick Lindemann <patrick.lindemann.99@gmail.com>
2023-06-18 20:54:28 +0200

    Added configurable serializer options

--- packages/query/src/serializer.ts
+++ packages/query/src/serializer.ts
@@ -1,9 +1,21 @@
 import { ParseTree, Serializer } from '@qliaison/core';
-import { QueryVisitor } from './visitor';
+import { QueryVisitor, QueryVisitorOptions } from './visitor';
+
+export type QuerySerializerOptions = QueryVisitorOptions;
 
 export class QuerySerializer extends Serializer<string> {
+  options: QuerySerializerOptions = {
+    parantheses: false,
+    condesed: false
+  };
+
+  constructor(options: Partial<QuerySerializerOptions> = {}) {
+    super();
+    this.options = { ...this.options, ...options };
+  }
+
   serialize(parseTree: ParseTree): string {
-    const visitor = new QueryVisitor();
+    const visitor = new QueryVisitor(this.options);
     return visitor.visitRoot(parseTree);
   }
 }
--- packages/query/src/visitor.ts
+++ packages/query/src/visitor.ts
@@ -11,7 +11,40 @@ import {
 } from '@qliaison/core';
 import { symbols } from './grammar';
 
+export interface QueryVisitorOptions {
+  /**
+   * Whether to wrap operations in parantheses or not.
+   *
+   * Example (parantheses = false):
+   *  `a = 1 AND b = 2 OR c = 3;`
+   *
+   * Example: (parantheses = true):
+   *  `((a = 1 AND b = 2) OR c = 3);`
+   *
+   */
+  parantheses: boolean;
+
+  /**
+   * Whether to add whitespaces to listings for better readability or not.
+   *
+   * Example (condensed = false):
+   * `a = func(1, false) or b in ['admin', 'user', 'guest']`
+   *
+   * Example (condesed = true):
+   * `a = func(1,false) or b in ['admin','user','guest']`
+   *
+   */
+  condesed: boolean;
+}
+
 export class QueryVisitor extends Visitor<string> {
+  options: QueryVisitorOptions;
+
+  constructor(options: QueryVisitorOptions) {
+    super();
+    this.options = options;
+  }
+
   visitRoot(root: Root): string {
     if (!root.child) {
       return '';
@@ -22,21 +55,23 @@ export class QueryVisitor extends Visitor<string> {
   visitUnaryOperation(operation: UnaryOperation): unknown {
     const operator = symbols[operation.operator];
     const value = operation.right.accept(this);
-    return `${operator} ${value}`;
+    const result = `${operator} ${value}`;
+    return this.options.parantheses ? `(${result})` : result;
   }
 
   visitBinaryOperation(operation: BinaryOperation): unknown {
     const operator = symbols[operation.operator];
     const left = operation.left.accept(this);
     const right = operation.right.accept(this);
-    return `(${left} ${operator} ${right})`;
+    const result = `${left} ${operator} ${right}`;
+    return this.options.parantheses ? `(${result})` : result;
   }
 
   visitFunction(func: Function): unknown {
     const identifier = func.identifier;
     const parameters = func.parameters
       .map((param) => param.accept(this))
-      .join(',');
+      .join(',' + (this.options.condesed ? '' : ' '));
     return `${identifier}(${parameters})`;
   }
 
@@ -45,7 +80,13 @@ export class QueryVisitor extends Visitor<string> {
   }
 
   visitArray<T extends AstNode>(array: Array<T>): unknown {
-    return '[' + array.items.map((item) => item.accept(this)) + ']';
+    return (
+      '[' +
+      array.items
+        .map((item) => item.accept(this))
+        .join(',' + (this.options.condesed ? '' : ' ')) +
+      ']'
+    );
   }
 
   visitValue(value: Value): unknown {
